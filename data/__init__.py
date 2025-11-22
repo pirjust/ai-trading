a01769c0f2051d82556383574d26c78a100cb5ef
pirjust <pirjust886@gmail.com>
2025-11-22 18:53:31 +0800

    Delete data directory

--- data/__init__.py
+++
@@ -1,7 +0,0 @@
-"""数据采集模块"""
-
-from .base_collector import BaseCollector
-from .websocket_collector import WebSocketCollector
-from .rest_collector import RestCollector
-
-__all__ = ["BaseCollector", "WebSocketCollector", "RestCollector"]
